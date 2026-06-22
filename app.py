0561726de98810662e8351f4f6fa410eed2f0c48
KEERTHIKA K <keerthikakaruna2005@gmail.com>
2026-06-22 13:30:07 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("AI-Based Smart Water Management Dashboard")
+
+st.metric("Current Water Usage", "210 L")
+st.metric("Predicted Usage", "269 L")
+
+st.warning("⚠ High Water Consumption Predicted")
+
+st.write("Recommendation: Implement water-saving measures and monitor usage.")
